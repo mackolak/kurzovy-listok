d3704ac55a64658314b7222f6e66fec2d1f2a898
Anna Cahojova <anna.cahojova@sk.ibm.com>
2022-05-15 23:54:21 +0200

    Add tests for missing input

--- test/kurzovy-listok.test.js
+++ test/kurzovy-listok.test.js
@@ -6,7 +6,7 @@ describe('Kurzovy Listok', () => {
     beforeEach(sandbox.restore);
 
     context('when data is valid', () => {
-      it('responds with 200 and list of converted amounts', async () => {
+      it('responds with 200 OK and list of converted amounts', async () => {
         const axiosStub = sandbox.stub(axios, 'get').resolves({
           data: {
             base_code: 'USD',
@@ -25,6 +25,26 @@ describe('Kurzovy Listok', () => {
         expect(text).to.eq(
           '{"EUR":"Converted amount for currency code EUR is 901.3 with rate 0.9013","USD":"Converted amount for currency code USD is 1000 with rate 1"}'
         );
+      });
+    });
+
+    context('when data is invalid', () => {
+      it('responds with 400 Bad Request Error when no currency code is provided', async () => {
+        const { text } = await request(app)
+          .post('/kurzovy-listok')
+          .send({ amount: 1000 })
+          .expect(400);
+
+        expect(text).to.eq('Invalid amount or currency');
+      });
+
+      it('responds with 400 Bad Request Error when no currency code is provided', async () => {
+        const { text } = await request(app)
+          .post('/kurzovy-listok')
+          .send({ currencyCode: 'EUR' })
+          .expect(400);
+
+        expect(text).to.eq('Invalid amount or currency');
       });
     });
   });
