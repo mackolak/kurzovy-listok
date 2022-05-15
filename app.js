19ef45b80d2c8bba910ec68d24be4b31c8652efc
Anna Cahojova <anna.cahojova@sk.ibm.com>
2022-05-15 22:58:13 +0200

    Add kurzovy-listok route and setup express

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const config = require('config');
+const bodyParser = require('body-parser');
+const port = config.get('port');
+const kurzovyListokController = require('./app/controllers/kurzovy-listok-controller');
+
+const app = express();
+
+app.use('/kurzovy-listok', bodyParser.json(), kurzovyListokController.index);
+
+app.use((req, res, next) => {
+  throw new Error('404 error');
+});
+
+app.use((error, req, res, next) => {
+  console.error(error.stack);
+  res.status(error.status || 500).send(error.message);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port: ${port} !`);
+});
+
+module.exports = app;
