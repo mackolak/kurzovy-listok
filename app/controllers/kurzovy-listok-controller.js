111372af90df14230ef9047ae27b24ec1abbff47
Anna Cahojova <anna.cahojova@sk.ibm.com>
2022-05-17 17:51:13 +0200

    Add date for converion rates to kurzovy-listok API

--- app/controllers/kurzovy-listok-controller.js
+++ app/controllers/kurzovy-listok-controller.js
@@ -1,16 +1,24 @@
 const KurzovyListokService = require('../services/kurzovy-listok-service');
+const formatCurrencyDate = require('../helpers/formatDate');
 const { BadRequestError } = require('../../lib/errors');
 
 class KurzovyListokController {
   static async index(req, res, next) {
     try {
-      const { suma: amount, kodMeny: currencyCode } = req.body;
-      if (!amount || !currencyCode) {
-        throw new BadRequestError('Suma alebo kod meny je nespravny');
+      const {
+        suma: amount,
+        kodMeny: currencyCode,
+        datumKonverznehoListka: currencyRateDate,
+      } = req.body;
+      if (!amount || !currencyCode || !currencyRateDate) {
+        throw new BadRequestError(
+          'Suma alebo kod meny alebo datum konverzneho listka je nespravny'
+        );
       }
       const kurzovyListok = await KurzovyListokService.getCurrenciesAndConvert(
         amount,
-        currencyCode
+        currencyCode,
+        formatCurrencyDate(currencyRateDate)
       );
       res.status(200).send(kurzovyListok);
     } catch (error) {
